b28a639bb475e50c330488101dead8854c7a00d2
yash0922 <yashkumarpal987@gmail.com>
2025-05-09 13:41:36 +0530

    udpate code

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,21 +4,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  // Replace with your Firebase config
-  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  // authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  // projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  // storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
   
-  apiKey: "AIzaSyADM9wJs1JC21ne9NVlziTjUinKcRGUvtM",
-  authDomain: "pdf-downloader-674b6.firebaseapp.com",
-  projectId: "pdf-downloader-674b6",
-  storageBucket: "pdf-downloader-674b6.firebasestorage.app",
-  messagingSenderId: "201402845829",
-  appId: "1:201402845829:web:7a6177862bdde14603b7ec",
-  measurementId: "G-1J2565VRBJ"
+
 };
 
 const app = initializeApp(firebaseConfig);
